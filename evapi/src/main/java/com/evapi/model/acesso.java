9baf80201c9d43686d4a9955434bc20f5d3d9a52
robsonCabral78 <robsoncabral_cabral@hotmail.com>
2024-03-13 17:01:44 -0300

    criação classe acesso

---
+++ evapi/src/main/java/com/evapi/model/acesso.java
@@ -0,0 +1,70 @@
+package com.evapi.model;
+
+import java.util.Objects;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "acesso")
+@SequenceGenerator(name="seq_acesso", sequenceName = "seq_acesso", allocationSize = 1, initialValue = 1)
+public class acesso implements GrantedAuthority {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_acesso")
+	private Long id;
+	
+	@Column(nullable = false)
+	private String descricao;
+
+	@Override
+	public String getAuthority() {
+		// TODO Auto-generated method stub
+		return this.descricao;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		acesso other = (acesso) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
