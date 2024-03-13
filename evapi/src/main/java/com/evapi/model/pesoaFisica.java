3655c31b97e7fc3ccc607163a47711a0c31f2a66
robsonCabral78 <robsoncabral_cabral@hotmail.com>
2024-03-13 20:42:11 -0300

    implementação classes pessoa, endereço, pessoa fisica, pessoa juridica.

---
+++ evapi/src/main/java/com/evapi/model/pesoaFisica.java
@@ -0,0 +1,44 @@
+package com.evapi.model;
+
+import java.util.Date;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table(name = "pessoa_fisica")
+@PrimaryKeyJoinColumn(name = "id")
+public class pesoaFisica extends pessoa {
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Column(nullable = false)
+	private String cpf;
+	
+	@Temporal(TemporalType.DATE)
+	private Date dataNacimento;
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Date getDataNacimento() {
+		return dataNacimento;
+	}
+
+	public void setDataNacimento(Date dataNacimento) {
+		this.dataNacimento = dataNacimento;
+	}
+
+	
+	
+
+}
---
+++ evapi/src/main/java/com/evapi/model/pessoaJuridica.java
@@ -0,0 +1,49 @@
+package com.evapi.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pessoa_juridica")
+@PrimaryKeyJoinColumn(name = "id")
+public class pessoaJuridica extends pessoa {
+
+
+	private static final long serialVersionUID = 1L;
+	
+	@Column(nullable = false)
+	private String cnpj;
+	
+	private String nomeFantasia;
+	
+	private String categoria;
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getNomeFantasia() {
+		return nomeFantasia;
+	}
+
+	public void setNomeFantasia(String nomeFantasia) {
+		this.nomeFantasia = nomeFantasia;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+
+}
